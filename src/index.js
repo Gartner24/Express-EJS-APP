09d3e1ec371f6aba5da45aaed8ef26562fd76722
Santiago <santiagovalencialeon@gmail.com>
2023-06-29 17:28:44 -0500

    feature: added some html

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import indexRoutes from './routes/routes.js';
 
 const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(indexRoutes)
+app.use(indexRoutes);
 
+app.use(express.static(join(__dirname, 'public')));
 
 app.listen(3000);
